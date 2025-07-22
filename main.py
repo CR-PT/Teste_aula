cbea5fceae706034114d7e40ffe9647e21ed7dfc
CR-PT <carlosarmando6@gmail.com>
2025-07-22 11:54:57 +0100

    Update main.py
    Seja bem-vindo à aula!

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Este script dá as boas-vindas à turma de Python
 
 print("Olá turma do Python!")  # Mensagem inicial
+print("Seja bem-vindo à aula!")
 print("Como estás?")  # Pergunta informal
 
 # Solicita o nome do usuário
