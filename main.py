f548e26e19de88e0c343622086a1e8c7ee530b68
CR-PT <carlosarmando6@gmail.com>
2025-07-22 11:51:34 +0100

    Update main.py
    Solicita o nome do usuário

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 print("Olá turma do Python!")  # Mensagem inicial
 print("Como estás?")  # Pergunta informal
 
-# Solicita o nome do utilizador
+# Solicita o nome do usuário
 nome: str = input("Qual é o seu nome? ")
 
 # Resposta personalizada
