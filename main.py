cd36a3695c41b461a672565ae8a7c662ce798502
CR-PT <carlosarmando6@gmail.com>
2025-07-22 11:43:32 +0100

    Update main.py
    adicionados comentários explicativos

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-print("Olá turma do Python!")
-print("Tudo bem!")
+# Este script dá as boas-vindas à turma de Python
+
+print("Olá turma do Python!")  # Mensagem inicial
+print("Tudo bem!")  # Pergunta informal
+
+# Solicita o nome do utilizador
 nome: str = input("Qual é o seu nome? ")
+
+# Resposta personalizada
 print(f"Olá, {nome}!")
